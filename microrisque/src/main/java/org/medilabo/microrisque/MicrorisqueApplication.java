c431873f051b5b3e28de8c5a6005f18f11507760
Barbara Samuel <barbara.samuel@yahoo.fr>
2025-05-30 20:38:54 +0200

    Ajout de la base du microservice microrisque

---
+++ microrisque/src/main/java/org/medilabo/microrisque/MicrorisqueApplication.java
@@ -0,0 +1,13 @@
+package org.medilabo.microrisque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrorisqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrorisqueApplication.class, args);
+	}
+
+}
